48f73fbdbfa94c71df01a2b071867ad157f5f63c
Manuel Cebreiro <104387536+ManuelCebreiro@users.noreply.github.com>
2022-11-09 12:04:13 +0100

    errores

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
                         Bienvenido a mi portfolio personal
                     </h3>
                     <p>Soy Manuel Cebreiro. Hace 10 años tuve mi primer encuentro con este mundo de la programación, y de manera autodidacta, aprendí 
-                        <strong><i> HTML</i></strong> y <strong><i>CSSS</i></strong> por circunstancias de la vida lo dejé apartado, hasta este año 2022, que
+                        <strong><i> HTML</i></strong> y <strong><i>CSS</i></strong> por circunstancias de la vida lo dejé apartado, hasta este año 2022, que
                         decidí dejar mi vida profesional actual para dar un cambio radical y dedicarme 100% a la programación. Cursando el bootcamp de <a href='https://4geeksacademy.com/es/inicio'>4GeeksAcademy</a>, he logrado encaminarme en la dirección que tanto soñaba. 
                         <br/><br/>La mejor decisión de mi vida.</p>
                 </div>
