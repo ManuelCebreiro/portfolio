af519ac0b053f1dba3c0b9110983c929d0b87573
Manuel Cebreiro <104387536+ManuelCebreiro@users.noreply.github.com>
2022-12-01 10:22:24 +0100

    probando formulario

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -5,10 +5,12 @@ const Formulario = () => {
     return (
         <div className='contenedor'>
             <div className='formulario'>
-                Contacto
-                <input type="text" name='name' placeholder='Nombre'></input>
-                <input type="text" name="email" placeholder='Email'></input>
-                <textarea type="text" placeholder='Tu mensaje'></textarea>
+                <div className='contenido'>
+                <input className='inputstyle' type="text" name='name' placeholder='Nombre'></input>
+                <input className='inputstyle' type="text" name="email" placeholder='Email'></input>
+                <textarea className='textareastyle' type="text" placeholder='Tu mensaje'></textarea>
+                <button className='btnenviar'>Enviar</button>
+                </div>
             </div>
         </div>
     )
